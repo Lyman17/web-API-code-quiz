63bfc3677531d57935aca1c01a6ebc83c60fe495
BLyman <135188980+Lyman17@users.noreply.github.com>
2023-07-22 18:20:48 -0600

    added more variables to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
+var quizStatus = true; // Know the status of the quiz. Quiz is not running = false , running = true
+var questionNumber = 0; // Track the question answered.
+var answerNumber = 0; // Track next answers to show
+var score = 0; // Max value by decreasing each wrong answer
+var highScore = 50; // Score add fix for ticking timer.
+var finalAnswerCheck = 0 // If last answer was wrong it will be validated outside of the time interval and then display as enabled = 1 
+var checkTimes = 1 // Check timer times call for function on last question
+
 var button1234 = document.createElement("button"); // Test answer 1
 var timeLeft = 60; // Global time left assignment counter
